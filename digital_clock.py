b7884fac18c1db995cddee611bff5bab1ce23a67
suraj99-shivank <shivanksuraj99@gmail.com>
2026-06-11 00:23:59 +0530

    digital clock

--- digital_clock.py
+++ digital_clock.py
@@ -1,14 +1,14 @@
 import tkinter as tk
-from time import strftime   # import strftime directly
+from time import strftime
 
 root = tk.Tk()
 root.title("Digital Clock")
 
 
 def update_time():
-    string = strftime("%H:%M:%S %p\n%D")  # fixed format string
+    string = strftime("%H:%M:%S %p\n%D")
     label.config(text=string)
-    label.after(1000, update_time)        # call itself every 1 sec
+    label.after(1000, update_time)
 
 
 label = tk.Label(root, font=("ds-digital", 80),
